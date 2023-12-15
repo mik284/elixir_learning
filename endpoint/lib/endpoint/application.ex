b1b1d23399d896943f3125798b6aabed1e3db22b
mik284 <mikeykamau222@gmail.com>
2023-12-15 18:44:58 +0300

    start endpoint install deps

---
+++ endpoint/lib/endpoint/application.ex
@@ -0,0 +1,20 @@
+defmodule Endpoint.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Endpoint.Worker.start_link(arg)
+      # {Endpoint.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Endpoint.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
