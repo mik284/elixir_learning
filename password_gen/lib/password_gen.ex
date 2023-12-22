3c9727975dad0e84c2e864620e2057f2620b208b
mik284 <mikeykamau222@gmail.com>
2023-12-22 09:49:36 +0300

    documented the module

--- password_gen/lib/password_gen.ex
+++ password_gen/lib/password_gen.ex
@@ -1,18 +1,41 @@
 defmodule PasswordGen do
   @moduledoc """
-  Documentation for `PasswordGen`.
+  Generates random password depeding on the parameters, module main function id `genrate(options)`
+  That function takes the options map.
+  options example:
+      options = %{
+        "length" => "5",
+        "numbers" => "false",
+        "uppercase" => "false",
+        "symbols" => "false"
+      }
+    The options are only 4, `length`, `numbers`, `uppercase`, `symbols`.
   """
-
+  @allowed_options [:length, :numbers, :uppercase, :symbols]
   @doc """
-  Hello world.
+  Generate password for given options:
 
   ## Examples
+      options = %{
+         "length" => "5",
+        "numbers" => "false",
+        "uppercase" => "false",
+        "symbols" => "false"
+      }
+      iex> PasswordGen.generate(options)
+      "abcdf"
 
-      iex> PasswordGen.hello()
-      :world
+       options = %{
+         "length" => "5",
+        "numbers" => "true",
+        "uppercase" => "false",
+        "symbols" => "false"
+      }
 
+      iex> PasswordGen.generate(options)
+      "abcdf45"
   """
-  def hello do
-    :world
-  end
+      def generate(options) do
+
+      end
 end
