2e4cb938c498868ab20c4117579c1298894fe5b8
mik284 <mikeykamau222@gmail.com>
2023-12-24 23:14:37 +0300

    using case instead of with

--- conditions/withcondition.ex
+++ conditions/withcondition.ex
@@ -1,8 +1,10 @@
 defmodule WithCondition do
   defp extract_login(%{"login" => login}), do: {:ok, login}
   defp extract_login(_), do: {:error, "login missing"}
+
   defp extract_email(%{"email" => email}), do: {:ok, email}
   defp extract_email(_), do: {:error, "email missing"}
+
   defp extract_password(%{"password" => password}), do: {:ok, password}
   defp extract_password(_), do: {:error, "password missing"}
 
