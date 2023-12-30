14e62f03a37915e660043dc8d9f5fa186cced35a
mik284 <mikeykamau222@gmail.com>
2023-12-31 00:28:06 +0300

    Gueard clause :negative

--- LoopsandIterations/natural_nums.ex
+++ LoopsandIterations/natural_nums.ex
@@ -1,7 +1,7 @@
 defmodule NaturalNums do
     def print(1), do: IO.puts(1)
 
-    def print(n) when is_number(n) do
+    def print(n) when is_number(n) and n > 1 do
             print(n - 1)
             IO.puts(n)
     end
