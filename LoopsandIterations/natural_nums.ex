8d11e4ce17b27395e3e26a853f89514748d1bdbe
mik284 <mikeykamau222@gmail.com>
2023-12-31 00:02:27 +0300

    add guard clause

--- LoopsandIterations/natural_nums.ex
+++ LoopsandIterations/natural_nums.ex
@@ -1,7 +1,12 @@
 defmodule NaturalNums do
     def print(1), do: IO.puts(1)
-        def print(n) do
+
+    def print(n) when is_number(n) do
             print(n - 1)
             IO.puts(n)
-        end
+    end
+
+  def print(_args), do: {:error , "Invalid input"}
+
+
 end
